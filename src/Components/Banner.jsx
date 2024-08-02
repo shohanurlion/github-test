5e4ac6ca87fbc9c34c22baad034ae229ee28c4aa
Nobin <mdnobin.fdr@gmail.com>
2024-08-02 22:38:02 +0600

    Nobin Banner change

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -8,6 +8,8 @@ const Banner = () => {
         <img className='w-full' src={banner} alt="" />
         <div className="text-yellow-950">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat nesciunt ducimus nemo laboriosam optio odio sapiente nam, laborum quos eum!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos impedit temporibus pariatur saepe, vel molestias, illo ipsam, reiciendis ad dolore iusto quo culpa voluptate ipsa ullam labore distinctio esse rerum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda molestiae accusamus rem, possimus odio commodi eveniet, nobis consequuntur expedita deserunt laboriosam eum itaque sapiente quisquam eaque? Aperiam asperiores odit voluptatum?
         </div>
     </div>
     </>
