c2f1eb041d6f42eebb49b36702a24f04e7f0e141
Nobin <mdnobin.fdr@gmail.com>
2024-08-02 22:16:49 +0600

    Nobin banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/NavBar/Navbar'
+import Banner from './Components/Banner'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <Navbar/>
+    <Banner />
     </>
   )
 }
---
+++ src/Components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import banner from '../assets/banner.png'
+
+const Banner = () => {
+  return (
+    <>
+    <div className="">
+        <img className='w-full' src={banner} alt="" />
+    </div>
+    </>
+  )
+}
+
+export default Banner
