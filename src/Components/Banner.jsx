953c73c4f8777dccf5a64ea5617216296b07f267
Nobin <mdnobin.fdr@gmail.com>
2024-08-02 22:24:34 +0600

    Nobin lorem

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -6,6 +6,9 @@ const Banner = () => {
     <>
     <div className="">
         <img className='w-full' src={banner} alt="" />
+        <div className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat nesciunt ducimus nemo laboriosam optio odio sapiente nam, laborum quos eum!
+        </div>
     </div>
     </>
   )
